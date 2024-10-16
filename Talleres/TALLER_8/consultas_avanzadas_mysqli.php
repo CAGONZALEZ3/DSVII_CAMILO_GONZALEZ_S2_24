a4baa35febc3f63f7a2582d7dcb0e9909819e2a1
CAGONZALEZ3 <camiloc.j03@gmail.com>
2024-10-15 21:30:07 -0500

    resuelto tarea 3

--- Talleres/TALLER_8/consultas_avanzadas_mysqli.php
+++ Talleres/TALLER_8/consultas_avanzadas_mysqli.php
@@ -55,6 +55,80 @@ if ($result) {
 } else {
     echo "Error: " . mysqli_error($conn);
 }
+//tarea
+echo "<h3>Tarea 1</h3>";
+$sql = "SELECT p.titulo, u.nombre as autor, p.fecha_publicacion 
+        FROM publicaciones p 
+        INNER JOIN usuarios u ON p.usuario_id = u.id 
+        ORDER BY p.fecha_publicacion DESC
+        LIMIT 5";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+    echo "<h3>Ultimas 5 publicaciones con nombre de autor y fecha de publicacionr:</h3>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "Título: " . $row['titulo'] . ", Autor: " . $row['autor'] . ", Fecha: " . $row['fecha_publicacion'] . "<br>";
+    }
+    mysqli_free_result($result);
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+echo "<h3>Tarea 2</h3>";
+$sql = "SELECT nombre, email
+        FROM USUARIOS
+        where id not in (select usuario_id from publicaciones)";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+    echo "<h3>Usuarios que no tienen publicaciones:</h3>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "Nombre: " . $row['nombre'] . ", Autor: " . $row['email']."<br>";
+    }
+    mysqli_free_result($result);
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+echo "<h3>Tarea 3</h3>";
+$sql = "SELECT count(*) cuantas,COUNT(p.id) / COUNT(DISTINCT u.id) promedio
+        FROM USUARIOS u
+        LEFT JOIN PUBLICACIONES p ON u.id = p.usuario_id";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+    echo "<h3>Promedio de publicaciones de usuario:</h3>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "Total Publicaciones: " . $row['cuantas'] . ", Promedio por Usuario: " . $row['promedio']."<br>";
+    }
+    mysqli_free_result($result);
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+echo "<h3>Tarea 4</h3>";
+$sql = "SELECT 
+            u.id AS usuario_id, 
+            u.nombre, 
+            MAX(p.fecha_publicacion) AS ultima_fecha_publicacion
+        FROM usuarios u
+        JOIN publicaciones p ON u.id = p.usuario_id
+        GROUP BY u.id";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+    echo "<h3>Ultima Publicacion por Usuario:</h3>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "Usuario: " . $row['nombre'] . ", Ultima Fecha de publicacion: " . $row['ultima_fecha_publicacion']."<br>";
+    }
+    mysqli_free_result($result);
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
 
 mysqli_close($conn);
 ?>
