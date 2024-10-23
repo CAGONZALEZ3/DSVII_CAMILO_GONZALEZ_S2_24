c57711d8c011ebb64a775cd1d8cd6a05c05f35e4
CAGONZALEZ3 <camiloc.j03@gmail.com>
2024-10-22 20:14:47 -0500

    con css

--- Parciales/PARCIAL3/index.php
+++ Parciales/PARCIAL3/index.php
@@ -19,29 +19,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notas</title>
-    
+    <link  rel="stylesheet" href="clases.css">
 </head>
 <body>
-    <?php echo "Usuario: ".$_SESSION['usuario']. "<br> rol: ".$_SESSION['rol']."<br>"; ?>
-    <table >
-        <thead>
-        <td>id</td>
-        <td>Usuario</td>
-        <td>Nota</td>
-        </thead>
-        <tbody>
-            <?php foreach($Notas as $nota): ?>
-                <?php if($_SESSION['usuario'] === $nota->usuario || $_SESSION['rol'] === 'PROFESOR'){ ?>
-                <tr>
-                <td><?php echo $nota->id; ?></td>
-                    <td><?php echo $nota->usuario; ?></td>
-                    <td><?php echo $nota->nota; ?></td>
-                </tr>
-                <?php }; ?>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    <a type="button" href="logout.php">logout</a>
     
+    <div class="containerG">
+    <?php echo "Usuario: ".$_SESSION['usuario']. "<br> rol: ".$_SESSION['rol']."<br>"; ?>
+        <div>
+            <table >
+                <thead>
+                <td>id</td>
+                <td>Usuario</td>
+                <td>Nota</td>
+                </thead>
+                <tbody>
+                    <?php foreach($Notas as $nota): ?>
+                        <?php if($_SESSION['usuario'] === $nota->usuario || $_SESSION['rol'] === 'PROFESOR'){ ?>
+                        <tr>
+                        <td><?php echo $nota->id; ?></td>
+                            <td><?php echo $nota->usuario; ?></td>
+                            <td><?php echo $nota->nota; ?></td>
+                        </tr>
+                        <?php }; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        
+        <div class="container">
+            <a type="button" class="button" href="logout.php">logout</a>
+        </div>
+    </div>
 </body>
 </html>
--- Parciales/PARCIAL3/login.php
+++ Parciales/PARCIAL3/login.php
@@ -45,38 +45,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-        header {
-            background-color: #333;
-            color: white;
-            padding: 15px 0;
-            text-align: center;
-        }
-        .container {
-            justify-content: center;
-            text-align: center;
-            align-items: center;
-            width: 80%;
-            margin: 5px;
-            overflow: hidden;
-            flex-direction: row;
-        }
-        .containerG {
-            justify-content: center;
-            text-align: center;
-            align-items: center;
-            display: flex;
-            flex-wrap: wrap;
-            align-content: center;
-            flex-direction: column;
-        }
-    </style>
+    <link  rel="stylesheet" href="clases.css">
 </head>
 <body>
     <form action="" method="post">
@@ -93,7 +62,7 @@
             <div class="container">
                 <input type="password" name="password_login" id="password_login" require>
             </div>
-            <button type="submit">Iniciar Sesion</button>
+            <button class="button" type="submit">Iniciar Sesion</button>
         </div>
     </form>
 </body>
